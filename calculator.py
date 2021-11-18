caea6988fae77ba6b0b793680533aa5f63d26095
Ivan <vsem2004@gmail.com>
2021-11-18 10:30:49 +0300

    Тесты для калькулятора

---
+++ calculator.py
@@ -0,0 +1,11 @@
+def sum_nums(x, y):
+    return x + y
+
+def subtraction_nums(x, y):
+    return x - y
+
+def multiply_nums(x, y):
+    return x * y
+
+def divide_nums(x, y):
+    return x / y
---
+++ test.py
@@ -0,0 +1,19 @@
+from calculator import sum_nums, subtraction_nums, multiply_nums, divide_nums
+
+def test_sums():
+    assert sum_nums(3, 5) == 8
+
+def test_subtraction():
+    assert subtraction_nums(9, 6) == 3
+
+def test_multiply():
+    assert multiply_nums(4, 3) == 12
+
+def test_divide():
+    assert divide_nums(10, 2) == 5
+
+
+test_sums()
+test_subtraction()
+test_multiply()
+test_divide()
